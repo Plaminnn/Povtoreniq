45e15de3f5457268b79db478460b6e29a7c488e0
Plaminnn <plamseizov09@gmail.com>
2024-12-13 09:07:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi edno chislo");
+            int n= int.Parse(Console.ReadLine());
+            var P = 1;
+            for (n = 1; n < 1; n++)
+            {
+                P = P * n;
+           
+            }
+            Console.WriteLine(P);
+        }
+    }
+}
